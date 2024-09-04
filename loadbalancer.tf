c14a75804b987b019b4623a2f8b43999a53c9084
cp <cp@pixx.io>
2024-09-04 17:17:54 +0200

    Add an internal load balancer as alternative to DNS entrys.

---
+++ loadbalancer.tf
@@ -0,0 +1,47 @@
+locals {
+  control_plane_lb_internal_ip = var.control_plane_internal_ip != null ? var.control_plane_internal_ip : cidrhost(var.node_ipv4_cidr, 5)
+}
+
+resource "hcloud_load_balancer" "control_plane" {
+  name               = "control_plane_lb"
+  load_balancer_type = var.control_plane_lb_type
+  network_zone       = "eu-central"
+  delete_protection  = true
+  labels = {
+    "cluster" = var.cluster_name,
+    "role"    = "control-plane"
+  }
+}
+
+resource "hcloud_load_balancer_network" "control_plane" {
+  load_balancer_id        = hcloud_load_balancer.control_plane.id
+  network_id              = hcloud_network.this.id
+  ip                      = local.control_plane_lb_internal_ip
+  enable_public_interface = var.enable_control_plane_lb_public_ip
+  depends_on = [
+    hcloud_network_subnet.nodes
+  ]
+}
+
+resource "hcloud_load_balancer_service" "control_plane_kubeapi" {
+  load_balancer_id = hcloud_load_balancer.control_plane.id
+  protocol         = "tcp"
+  listen_port      = 6443
+  destination_port = 6443
+  proxyprotocol    = true
+}
+
+resource "hcloud_load_balancer_service" "control_plane_talosapi" {
+  load_balancer_id = hcloud_load_balancer.control_plane.id
+  protocol         = "tcp"
+  listen_port      = 50000
+  destination_port = 50000
+  proxyprotocol    = true
+}
+
+resource "hcloud_load_balancer_target" "control_plane_target" {
+  type             = "label_selector"
+  load_balancer_id = hcloud_load_balancer.control_plane.id
+  label_selector   = "role=control-plane"
+  use_private_ip   = true
+}
